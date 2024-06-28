9cc5bc247048fdd07c6dd13c8fd0c3bca5c43c31
CreepyCat2005 <90690249+CreepyCat2005@users.noreply.github.com>
2024-06-28 11:26:04 +0200

    added file explorer for object selection

--- ImguiRenderer.cpp
+++ ImguiRenderer.cpp
@@ -1,6 +1,10 @@
 #include "ImguiRenderer.hpp"
 #include "ObjectLoader.hpp"
 
+#include <windows.h>
+#include <commdlg.h>
+#include <iostream>
+
 void ImguiRenderer::renderMenuBar()
 {
 	if (ImGui::BeginMainMenuBar())
@@ -125,10 +129,32 @@ void ImguiRenderer::renderMenuBar()
 		{
 			if (ImGui::Button("Test File"))
 			{
-				Mesh mesh;
+				OPENFILENAMEA ofn;       // common dialog box structure
+				char szFile[260];       // buffer for file name
+				HWND hwnd = NULL;       // owner window
+				HANDLE hf;              // file handle
 
-				ObjectLoader::load_model_mesh_assimp(".\\Assets\\Objects\\Cube.obj", &mesh);
-				auto test = 0;
+				// Initialize OPENFILENAME
+				ZeroMemory(&ofn, sizeof(ofn));
+				ofn.lStructSize = sizeof(ofn);
+				ofn.hwndOwner = hwnd;
+				ofn.lpstrFile = szFile;
+				// Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+				// use the contents of szFile to initialize itself.
+				ofn.lpstrFile[0] = '\0';
+				ofn.nMaxFile = sizeof(szFile);
+				ofn.lpstrFilter = "All\0*.*\0Text\0*.TXT\0";
+				ofn.nFilterIndex = 1;
+				ofn.lpstrFileTitle = NULL;
+				ofn.nMaxFileTitle = 0;
+				ofn.lpstrInitialDir = NULL;
+				ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+				// Display the Open dialog box
+				if (GetOpenFileNameA(&ofn) == TRUE) 
+				{
+					auto test = ofn.lpstrFile;
+				}
 			}
 
 			ImGui::EndMenu();
